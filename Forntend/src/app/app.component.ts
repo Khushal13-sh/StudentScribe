4313f201e948aaf9215dd1a9ed2194a3a67115ee
Khushal Sherki <154081520+Khushal13-sh@users.noreply.github.com>
2024-04-27 17:09:49 +0530

    Add files via upload

---
+++ Forntend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+ import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent 
+{  
+  title: any;
+
+  isLoggedIn:number=0; //0=No 1==yes
+}
